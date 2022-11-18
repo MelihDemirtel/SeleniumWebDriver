2d0aeec495bff9fdf942ddcb71711f2482fdcca3
MelihDemirtel <melihcandemirtel@gmail.com>
2022-11-18 12:58:18 +0300

    created navigateMethods package and DriverNavigateMethods java class

--- Selenium/src/test/java/manageMethods/DriverManageMethods.java
+++ Selenium/src/test/java/manageMethods/DriverManageMethods.java
@@ -1,4 +1,42 @@
 package manageMethods;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Time;
+
 public class DriverManageMethods {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();//Chrome browser kurulumu
+        WebDriver cdriver = new ChromeDriver();//Chrome driver çalıştırmak için
+        int milis = 2000;//2 saniye bekleme süresi
+
+        cdriver.get("https://www.google.com");
+        Thread.sleep(milis);
+
+        cdriver.manage().window().maximize();//Browser'ı tam ekran yapar
+        Thread.sleep(milis);//Bekleme adımı için gerekli
+
+        System.out.println(cdriver.manage().window().getSize());//Browser penceresinin boyutlarını verir
+        Thread.sleep(milis);
+
+        cdriver.manage().window().setSize(new Dimension(640,360));//Browser penceresinin boyutunu belirlemek için
+        Thread.sleep(milis);
+
+        cdriver.manage().window().setPosition(new Point(100,100));//Browser penceresinin açılma konumu koordinatlarını belirler
+        Thread.sleep(milis);
+
+        System.out.println(cdriver.manage().window().getPosition());//Browser penceresinin açılma konumu koordinatlarını verir
+        Thread.sleep(milis);
+
+        cdriver.manage().window().fullscreen();//Browser penceresini full ekran yapar
+        Thread.sleep(milis);
+
+        cdriver.quit();//Browser'ı kapatır
+
+    }
 }
---
+++ Selenium/src/test/java/navigateMethods/DriverNavigateMethods.java
@@ -0,0 +1,17 @@
+package navigateMethods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverNavigateMethods {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();//Chrome browser kurulumu
+        WebDriver cdriver = new ChromeDriver();//Chrome driver çalıştırmak için
+        int milis = 2000;
+
+        cdriver.get("https://www.google.com");
+
+    }
+}
